c861a72bf19ff98e02e268c98166c81b4968c9ea
Ananda Ricky Fauzi <anandaricky01@gmail.com>
2022-01-19 12:34:29 +0700

    menambahkan nama jalan dan provinsi pada navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,15 +1,19 @@
-<div class="navbar navbar-expand-lg navbar-dark bg-primary" style="border-bottom: 1px solid #00008c;">
+<div class="navbar navbar-expand-lg navbar-dark bg-primary" style="border-bottom: 1px solid #578aff ;">
   <div class="container p-1">
   <a href="home" class="custom-logo-link" rel="home"><img src="https://dapo.kemdikbud.go.id/assets/img/logo-dikdasmen.png" alt="" class="img-responsive"></a>
     <div class="col text-left">
-      <h3 class="m-3 text-light">SDN 028 TENGGARONG SEBERANG</h3>
-      <p class="lead m-3 text-light">Terdepan dalam Imtaq dan Prestasi</p>
+      <h3 class="mt-3 ms-3 text-light">SDN 028 TENGGARONG SEBERANG</h3>
+      <p class="lead ms-3 text-light">
+        JL. Gunung Kombeng Tenggarong <br>
+        Kabupaten Kutai Kartanegara - Kalimantan Timur
+      </p>
     </div>
   </div>
 </div>
 
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #2626ff;">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary  bg-gradient">
   <div class="container">
+    <a class="navbar-brand" href="home">Dashboard Data</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
