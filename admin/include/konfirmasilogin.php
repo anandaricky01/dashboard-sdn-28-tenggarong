d978a874cead8a9bb895b6f9b766865741532c96
Ricky <anandaricky01@gmail.com>
2022-02-04 18:46:51 +0700

    memperbaiki algoritma login

--- admin/include/konfirmasilogin.php
+++ admin/include/konfirmasilogin.php
@@ -1,33 +1,43 @@
 <?php
-    if (isset($_POST['login'])) {
-        $user = $_POST['username'];
-        $pass = $_POST['password'];
-        $username = mysqli_real_escape_string($koneksi, $user);
-        $password = mysqli_real_escape_string($koneksi, MD5($pass));
-        
-        //cek username dan password
-        $sql="select `id_user`, `id_role` from `user` 
-                where `username`='$username' and
-               `password`='$password'";
-        $query = mysqli_query($koneksi, $sql);
-        $jumlah = mysqli_num_rows($query);
-        if(empty($user)){             
-             header("Location:index.php?include=login&gagal=userKosong");
-        }else if(empty($pass)){           
-             header("Location:index.php?include=login&gagal=passKosong");
-        }else if($jumlah==0){
-             header("Location:index.php?include=login&gagal=userpassSalah");
-        }
-        else{
+if (isset($_POST['login'])) {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    // periksa username
+    $sql_periksa = "SELECT * FROM `user` WHERE `username`='$user'";
+    $password_user = mysqli_fetch_assoc(mysqli_query($koneksi, $sql_periksa))['password'];
+
+    // password_verify($_POST['password'], $user['password']
+
+    $username = mysqli_real_escape_string($koneksi, $user);
+    $password = mysqli_real_escape_string($koneksi, $pass);
+
+    //cek username dan password
+    $sql = "select `id_user`, `id_role` from `user` 
+                where `username`='$username'";
+    $query = mysqli_query($koneksi, $sql);
+    $jumlah = mysqli_num_rows($query);
+    if (empty($user)) {
+        header("Location:index.php?include=login&gagal=userKosong");
+    } else if (empty($pass)) {
+        header("Location:index.php?include=login&gagal=passKosong");
+    } else if ($jumlah == 0) {
+        header("Location:index.php?include=login&gagal=userpassSalah");
+    } else {
+        // verifikasi
+        if (password_verify($password, $password_user)) {
+
             //get data
-            while($data = mysqli_fetch_row($query)){
+            while ($data = mysqli_fetch_row($query)) {
                 $id_user = $data[0]; //1
                 $level = $data[1]; //speradmin
-                $_SESSION['id_user']=$id_user;
-                $_SESSION['level']=$level;
+                $_SESSION['id_user'] = $id_user;
+                $_SESSION['level'] = $level;
                 header("Location:profil");
                 exit;
-            }           
+            }
+        } else {
+            header("Location:index.php?include=login&gagal=userpassSalah");
         }
     }
-?>
+}
--- admin/include/konfirmasitambahuser.php
+++ admin/include/konfirmasitambahuser.php
@@ -1,37 +1,31 @@
-<?php 
-   
-    $nama = $_POST['nama'];
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $password = md5($_POST['password']);
-    $level = $_POST['level'];
-    $lokasi_file = $_FILES['foto']['tmp_name'];
-    $nama_file = $_FILES['foto']['name'];
-    $direktori = 'foto/'.$nama_file;
- 
-    if(empty($nama)){
+<?php
+
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);;
+$level = $_POST['level'];
+
+if (empty($nama)) {
    header("Location:tambah-user&notif=tambahkosong&jenis=nama");
-   }else if(empty($email)){      
-      header("Location:tambah-user&notif=tambahkosong&jenis=email");
-   }else if(empty($username)){
+} else if (empty($email)) {
+   header("Location:tambah-user&notif=tambahkosong&jenis=email");
+} else if (empty($username)) {
    header("Location:tambah-user&notif=tambahkosong&jenis=username");
-   }else if(empty($password)){
+} else if (empty($password)) {
    header("Location:tambah-user&notif=tambahkosong&jenis=password");
-   }else if(empty($level)){
+} else if (empty($level)) {
    header("Location:tambah-user&notif=tambahkosong&jenis=level");
-   }else if(!move_uploaded_file($lokasi_file,$direktori)){
-      header("Location:tambah-user&notif=tambahkosong&jenis=foto");
-    }else{   
+} else {
 
    $sql = "INSERT INTO `user` 
       (`id_user`,`nama`,`email`,`username`,
-   `password`,`id_role`,`foto`)
+   `password`,`id_role`)
       VALUES (NULL,'$nama','$email','$username',
-   '$password','$level','$nama_file')";
-      //echo $sql;
-   mysqli_query($koneksi,$sql);
+   '$password','$level')";
+   //echo $sql;
+   mysqli_query($koneksi, $sql);
    $id_buku = mysqli_insert_id($koneksi);
- 
-      header("Location:user&notif=tambahberhasil");
-    }
-?>
+
+   header("Location:user&notif=tambahberhasil");
+}
